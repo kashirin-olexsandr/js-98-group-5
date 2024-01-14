6603863234a85bca990dc1ac9f5bcd0dc3cf1882
Saniokkk <kashirin.olexsandr91@gmail.com>
2024-01-14 13:01:43 +0200

    add task-7

--- js/lesson-4.js
+++ js/lesson-4.js
@@ -15,3 +15,12 @@ function handleClick() {
   leftSwapInput.value = valueRight;
   rightSwapInput.value = valueLeft;
 }
+
+/*
+Завдання 7
+При натисканні на коло він повинен слідувати за курсором.
+При повторному натисканні він стає в початкове положення.
+https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/MouseEvent
+https://developer.mozilla.org/ru/docs/Web/API/MouseEvent/pageX
+https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/pageY
+*/
